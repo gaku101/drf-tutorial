e784669e0ace7848539af29869c7cb864c280e77
takehiro <takehiro-r133@hotmail.co.jp>
2024-04-23 00:38:11 +0900

    add posts views

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import helloWorld
+from django.urls import path, include
+from .views import helloWorld, PostViewSet, getAllPosts
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
 
 urlpatterns = [
     path('hello/', helloWorld, name='hello'),
+    path('', getAllPosts, name='getAllPosts'),
+    path('', include(router.urls))
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 def helloWorld(request):
   # return HttpResponse('Hello World')
   return render(request, 'posts/index.html')
+
+@api_view(['GET'])
+def getAllPosts(request):
+  posts = Post.objects.all()
+
+  serializer = PostSerializer(posts, many=True)
+
+  return Response(serializer.data)
+
+class PostViewSet(viewsets.ModelViewSet):
+  queryset = Post.objects.all()
+  serializer_class = PostSerializer
